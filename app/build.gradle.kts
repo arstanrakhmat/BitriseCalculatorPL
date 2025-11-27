a93dc7254551f1e6116d9fae181f40cd3eaf84cf
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-11-27 23:16:25 +0600

    Added ExpressionParser.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -57,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    testImplementation(libs.test.core.testing)
+    testImplementation(libs.test.truth)
 }
