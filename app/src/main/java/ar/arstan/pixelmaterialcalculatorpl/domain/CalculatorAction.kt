5094cc94c83a9f5464bbbb75c888bd0b73fc9bf0
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-11-30 12:26:23 +0600

    Added ExpressionWriter. kt

---
+++ app/src/main/java/ar/arstan/pixelmaterialcalculatorpl/domain/CalculatorAction.kt
@@ -0,0 +1,18 @@
+package ar.arstan.pixelmaterialcalculatorpl.domain
+
+sealed interface CalculatorAction {
+
+    data class Number(val number: Int) : CalculatorAction
+
+    data class Op(val operation: Operation) : CalculatorAction
+
+    object Clear : CalculatorAction
+
+    object Delete : CalculatorAction
+
+    object Parentheses : CalculatorAction
+
+    object Calculate : CalculatorAction
+
+    object Decimal : CalculatorAction
+}
